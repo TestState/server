58a8aa1bd74e93edd5c8a5b51f494071148e7ad1
hsgamer <huynhqtienvtag@gmail.com>
2026-06-15 14:06:50 +0700

    fix: use BrowserRouter

--- src/main/webui/src/App.jsx
+++ src/main/webui/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter as Router, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {AppShell, Burger, createTheme, Group, MantineProvider, NavLink, Text} from '@mantine/core';
 import {useDisclosure} from '@mantine/hooks';
